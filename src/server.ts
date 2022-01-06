185c89a7b1910bc46691763e5a3f46bcb2931ec0
guilhermeSmartins <mineg4500@gmail.com>
2022-01-06 18:53:20 -0300

    swagger and database added to express server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import express, { Request, Response } from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
+import express from "express";
+import swaggerUi from 'swagger-ui-express';
+
+import { router } from "./routes";
+import swaggerFile from './swagger.json';
+
+import './database';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
 
-app.use('/categories', categoriesRoutes);
+
 app.listen(3333, () => console.log("running"));
