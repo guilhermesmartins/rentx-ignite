8ee7c99d18f708a24cf7b36610512ed1ced92fbf
guilhermeSmartins <mineg4500@gmail.com>
2022-01-24 20:37:22 -0300

    index for routes

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { categoriesRoutes } from './categories.routes';
+import { specificationsRoutes } from './specifications.routes';
+import { usersRouters } from './users.routes';
+
+export const router = Router();
+
+router.use('/categories', categoriesRoutes);
+router.use('/specifications', specificationsRoutes);
+router.use('/users', usersRouters);
