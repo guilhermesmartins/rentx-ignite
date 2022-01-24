854d3444e04ab193ccbf82d18a1b7c823ce691aa
guilhermeSmartins <mineg4500@gmail.com>
2022-01-24 20:32:39 -0300

    Specification entity and repository

---
+++ src/modules/Cars/entities/Specification.ts
@@ -0,0 +1,17 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('specifications')
+export class Specification {
+    @PrimaryGeneratedColumn('uuid')
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+    
+    @CreateDateColumn()
+    created_at: Date;
+}
