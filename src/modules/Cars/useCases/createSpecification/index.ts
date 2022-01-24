1c3e10aa58c5ee91ea1e9f5d169b3414853c1385
guilhermeSmartins <mineg4500@gmail.com>
2022-01-24 20:36:57 -0300

    createCategory and createSpefication index

---
+++ src/modules/Cars/useCases/createSpecification/index.ts
@@ -0,0 +1,7 @@
+import SpecificationsRepository from "../../Repositories/Implementations/SpecificationsRepository";
+import CreateSpecificationController from "./CreateSpecificationController";
+import CreateSpecificationUseCase from "./CreateSpecificationUseCase";
+
+const specificationsRepository = SpecificationsRepository.getInstance();
+export const createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepository);
+export const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase);
