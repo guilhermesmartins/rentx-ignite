7e5d4b148af20c5ba3d535ece1389556015039be
guilhermeSmartins <mineg4500@gmail.com>
2022-01-24 20:23:10 -0300

    migrations created for categories, specifications and users

---
+++ src/database/migrations/1641857321718-CreateSpecifications.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSpecifications1641857321718 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'specifications',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()'
+                        
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    }
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
