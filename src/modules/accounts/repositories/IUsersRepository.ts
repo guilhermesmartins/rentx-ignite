3cd3ed2cdaae3115e551b2ff7c03f5cf0ed6ee7e
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:06:43 -0300

    feat(IUsersRepository): find by email and find by id functions added to interface

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,8 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import User from "../entities/User";
 
 export default interface IUsersRepository {
-    create(data: ICreateUserDTO): Promise<void>;
+  create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
 }
