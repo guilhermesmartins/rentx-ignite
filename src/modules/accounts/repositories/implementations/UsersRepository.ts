7d3eb8cb67c4a35acf76768039ac4082c4f68621
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:05:13 -0300

    feat(UsersRepository): findbyemail and findbyid functions made

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,5 @@
 import { getRepository, Repository } from "typeorm";
+import { v4 } from 'uuid';
 
 import User from '../../entities/User';
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -11,15 +12,30 @@ export default class UsersRepository implements IUsersRepository {
         this.repository = getRepository(User);
     }
     
-    async create({ name, driver_license, email, password, username }: ICreateUserDTO): Promise<void> {
-        const user = this.repository.create({
-            name,
-            username,
-            email,
-            driver_license,
-            password
-        });
+  async create({ name, driver_license, email, password, username, id, avatar }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      id: v4(),
+      name,
+      username,
+      email,
+      driver_license,
+      isAdmin: false,
+      password,
+      avatar,
+    });
 
-        await this.repository.save(user);
-    }
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<undefined | User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    const user = await this.repository.findOne({ id });
+
+    return user;
+  }
 }
