cd95cafbd36d316d0c23e4ec3e3f3b08b2f253e2
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:09:42 -0300

    feat(users.routes): update on all user routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,23 @@
 import { Router } from 'express';
-import CreateUserController from '../modules/accounts/useCases/CreateUserController';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import CreateUserController from '../modules/accounts/useCases/createUser/CreateUserController';
+import UpdateUserAvatarController from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 export const usersRouters = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouters.post('/', createUserController.handle);
+
+usersRouters.patch(
+  '/', 
+  uploadAvatar.single("avatar"), 
+  ensureAuthenticated,
+  updateUserAvatarController.handle
+);
