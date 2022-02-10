67a857ffe4fe2c8185f0dba813412d2d86641fed
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:21:36 -0300

    feat(authenticate.routes): post route to get the token made

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import AuthenticateUserController from '../modules/accounts/useCases/authenticateUser/AuthenticateUserController';
+
+export const authenticateRoutes = Router();
+
+export const authenticateUserController = new AuthenticateUserController();
+
+authenticateRoutes.post('/', authenticateUserController.handle);
