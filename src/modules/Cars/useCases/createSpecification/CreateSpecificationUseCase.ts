e3161239abe5b896fedd37ce92dcb059995b3a63
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:25:11 -0300

    feat: UseCase and Repository for specifications made

--- src/modules/Cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/Cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,25 +1,29 @@
-import { inject, injectable } from "tsyringe";
-import { ISpecificationsRepository } from "../../Repositories/ISpecificationsRepository";
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '../../../../errors/AppError';
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
-    name: string;
-    description: string;
+  name: string;
+  description: string;
 }
 
 @injectable()
 export default class CreateSpecificationUseCase {
-    constructor(
-        @inject('SpecificationsRepository')
-        private specificationsRepository: ISpecificationsRepository
-    ) {}
+  constructor(
+    @inject('SpecificationsRepository')
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
 
-    async execute({ name, description }: IRequest): Promise<void> {
-        const specificationExists = await this.specificationsRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const specificationExists = await this.specificationsRepository.findByName(
+      name
+    );
 
-        if(specificationExists) {
-            throw new Error('Specification already exists');
-        }
-
-        await this.specificationsRepository.create({ name, description });
+    if (specificationExists) {
+      throw new AppError('Specification already exists');
     }
+
+    await this.specificationsRepository.create({ name, description });
+  }
 }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import CreateSpecificationController from '../modules/Cars/useCases/createSpecification/CreateSpecificationController';
 
 export const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post('/', createSpecificationController.handle);
