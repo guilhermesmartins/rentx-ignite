f0e178e48edaf0425a8fabf4dc3688774a8fa0cd
guilhermeSmartins <mineg4500@gmail.com>
2022-02-10 19:21:01 -0300

    feat(AuthenticateUserController,-AuthenticateUserUseCase): controller and useCase made

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { inject, injectable } from 'tsyringe';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
+import AppError from '@errors/AppError';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export default class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const token = sign({}, 'qg}poN6$!F8d7H', {
+      subject: user.id,
+      expiresIn: '1d'
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    };
+
+    return tokenReturn;
+  }
+}
