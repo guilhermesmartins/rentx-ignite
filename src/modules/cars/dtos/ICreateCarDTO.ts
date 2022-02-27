041162a74fbd2f30dc9d95711f1e33f74b87cd2e
guilhermeSmartins <mineg4500@gmail.com>
2022-02-27 07:24:54 -0300

    style(ICreateCarDTO): car dto

---
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -0,0 +1,9 @@
+export default interface ICreateCarDTO {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
