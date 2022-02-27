f57ce627adae61c87fd1c00ebf4a4fda1e2540f2
guilhermeSmartins <mineg4500@gmail.com>
2022-02-27 07:57:40 -0300

    style(specifications.route): style

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import CreateSpecificationController from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 export const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
