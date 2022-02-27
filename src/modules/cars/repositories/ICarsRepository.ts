0d26acb3602b32d4385a2238c19d5654ea675af1
guilhermeSmartins <mineg4500@gmail.com>
2022-02-27 07:38:12 -0300

    style(ICarsRepository): interface for car repository

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,8 @@
+import ICreateCarDTO from "../dtos/ICreateCarDTO";
+import Car from "../infra/typeorm/entities/Car";
+
+export default interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car | undefined>;
+  findAvaiable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
+}
